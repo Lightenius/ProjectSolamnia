1abf4c5a86763bcd2ca2f9120639c27ba83a4e6d
Lightenius <ahmet.cil.03@gmail.com>
2025-06-16 19:01:11 +0300

    Basic server strüktür hazırlandı.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using static Solamnia;
 using Microsoft.AspNetCore.Builder;
+using ProjectSolamnia.Server;
 
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Solamnia.Main(new string[]{});
+Solamnia.Main(new string[] { });
+
+SolamniaServer.Run();
+
--- server/Server.cs
+++ server/Server.cs
@@ -1,7 +1,33 @@
+using System;
 using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Razor;
+using Microsoft.Extensions.DependencyInjection;
 
+namespace ProjectSolamnia.Server;
 
 public class SolamniaServer
 {
-    
+    public static void Run()
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder();
+
+        builder.Services.AddRazorPages(); // TODO: 
+
+        WebApplication app = builder.Build();
+
+
+        app.MapGet("/{s}/{p}", handleDefault);
+
+        app.Run();
+
+    }
+
+    private static void handleDefault(string p, string s)
+    {
+        if (p != null && s != null)
+        {
+            Console.WriteLine(s + " " + p);
+        }
+    }
 }

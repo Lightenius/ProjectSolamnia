6b962c185d1cdb9a3cd322240b6d9bc82d7e944e
Lightenius <ahmet.cil.03@gmail.com>
2025-06-16 18:09:11 +0300

    update

--- obj/Debug/net9.0/ProjectSolamnia.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjectSolamnia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSolamnia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9d7b4cb63e7542c3dc3fb12a83f1e50d181264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1216fd594fe593283f57ff8c00f9eb0c0b8da123")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSolamnia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSolamnia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

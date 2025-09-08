a76e1ec42e0405a1e0b07b78ca4eede884c09ca0
RustyThing <ekivanc4w@gmail.com>
2025-09-08 12:23:36 +0300

    databsae sayısı 1e indirildi

--- obj/Debug/net9.0/ProjectSolamnia.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjectSolamnia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectSolamnia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1f7bb79f2bb945d924b5172be9929e21db688e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4e9f2d87384833cf74e550809be6bbb4699b90")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectSolamnia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectSolamnia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
